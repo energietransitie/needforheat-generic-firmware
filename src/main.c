ef2ae083a2131cc21f2b2365d5e7c778acc7c9d3
Kevin Jansen <kevin@vjkd.nl>
2021-04-09 23:49:51 +0200

    Added some comments for clarity

--- src/main.c
+++ src/main.c
@@ -12,16 +12,23 @@ void app_main(void)
     wifi_prov_mgr_config_t config = initialize_provisioning();
     //Starts provisioning if not provisioned, otherwise skips provisioning.
     start_provisioning(config);
-
+    //Initialize time with timezone Europe and city Amsterdam
     initialize_time("CET-1CEST-2,M3.5.0/02:00:00,M10.5.0/03:00:00");
+    //URL Do not forget to use https:// when using the https() function.
     char* url = "https://192.168.178.75:4444/set/house/opentherm";
+
+    //Gets time as epoch time.
     int now = time(NULL);
+
+    //Creates data string replacing %d with the time integer.
     char* dataPlain = "{\"deviceMac\":\"8C:AA:B5:85:A2:3D\",\"measurements\": [{\"property\":\"testy\",\"value\":\"hello_world\"}],\"time\":%d}";
     char data [strlen(dataPlain)];
     sprintf(data, dataPlain, now);
+
     /* Start main application now */
     while (1)
     {
+        //Logs hello world and tries to post with https every 10 seconds. Replace post_https(url, data, cert) with post_http(url,data) to use plain http over TCP
         ESP_LOGI(TAG, "Hello World!");
         vTaskDelay(10000 / portTICK_PERIOD_MS);
         post_https(url, data, NULL);
