c68f2265c3b0cdf9da6b9462a0098991051fb862
Henri ter Hofte <h.ter.hofte@windesheim.nl>
2021-05-29 21:45:58 +0200

    Update generic_esp_32.c
    remove all references to timezones other than UTC

--- lib/generic_esp_32/generic_esp_32.c
+++ lib/generic_esp_32/generic_esp_32.c
@@ -7,7 +7,6 @@ static const char *TAG = "Twomes Generic Firmware Library ESP32";
 bool activation = false;
 //Interrupt Queue Handler:
 static xQueueHandle gpio_evt_queue = NULL;
-// char *https_url = "192.168.178.75:4444/set/house/opentherm";
 
 /* Signal Wi-Fi events on this event-group */
 const int WIFI_CONNECTED_EVENT = BIT0;
@@ -402,7 +401,7 @@ void initialize_time(char *timezone)
     tzset();
     localtime_r(&now, &timeinfo);
     strftime(strftime_buf, sizeof(strftime_buf), "%c", &timeinfo);
-    ESP_LOGI(TAG, "The current date/time in Amsterdam is: %s", strftime_buf);
+    ESP_LOGI(TAG, "The current UTC date/time is: %s", strftime_buf);
 }
 
 void post_http(char *url, char *data, char *authenticationToken)
