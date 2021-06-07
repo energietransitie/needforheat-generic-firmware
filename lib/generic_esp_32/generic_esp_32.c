fb6a881532d2128dcd718e13594888e3994d97d9
Ghraemdal <ghraemdal@localhost.localdomain>
2021-06-08 00:09:24 +0200

    All caps device name integrated

--- lib/generic_esp_32/generic_esp_32.c
+++ lib/generic_esp_32/generic_esp_32.c
@@ -427,7 +427,7 @@ void get_device_service_name(char *service_name, size_t max)
     uint8_t eth_mac[6];
     const char *ssid_prefix = SSID_PREFIX;
     esp_wifi_get_mac(WIFI_IF_STA, eth_mac);
-    snprintf(service_name, max, "%s%02x%02x%02x",
+    snprintf(service_name, max, "%s%02X%02X%02X",
              ssid_prefix, eth_mac[3], eth_mac[4], eth_mac[5]);
 }
 
