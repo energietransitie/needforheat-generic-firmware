26b124086ee7c0c5381a00de1bb91b1b171a318d
Henri ter Hofte <h.ter.hofte@windesheim.nl>
2021-10-08 19:53:21 +0200

    change build flags for debug

--- src/main.c
+++ src/main.c
@@ -2,11 +2,12 @@
 
 #ifdef CONFIG_TWOMES_PRESENCE_DETECTION
 const char*device_type_name = "Presence-Detector";
+static const char *TAG = "Twomes ESP32 presence detector";
 #else
 const char*device_type_name = "Generic-Test";
+static const char *TAG = "Twomes ESP32 generic test device";
 #endif
 
-static const char *TAG = "Twomes ESP32 generic test device";
 
 #define BOOT_STARTUP_INTERVAL_MS (10 * 60) // milliseconds ( 10 s * 1000 ms/s)
 #define BOOT_STARTUP_INTERVAL_TXT "Wating 10 seconds before next measurement data series is started"
