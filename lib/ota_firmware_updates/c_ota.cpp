74c8e989516e1ecacc2287eab4aaad882efff3cd
n-vr <43145188+n-vr@users.noreply.github.com>
2022-04-29 13:04:16 +0000

    Add OTA firmware updates
    Over-The-Air firmware updates in C++. See c_ota.h to use this in C code.

---
+++ lib/ota_firmware_updates/c_ota.cpp
@@ -0,0 +1,28 @@
+#include "c_ota.h"
+#include "ota_firmware_updater.h"
+
+// This is an adapter so this C++ code can be called by C code.
+
+#ifdef __cplusplus
+extern "C"
+{
+#endif // __cplusplus
+
+    void twomes_ota_start()
+    {
+        OTAFirmwareUpdater::Start();
+    }
+
+    void twomes_ota_check()
+    {
+        OTAFirmwareUpdater::Check();
+    }
+
+    void twomes_ota_check_update_finished_successfully()
+    {
+        OTAFirmwareUpdater::CheckUpdateFinishedSuccessfully();
+    }
+
+#ifdef __cplusplus
+}
+#endif // __cplusplus
