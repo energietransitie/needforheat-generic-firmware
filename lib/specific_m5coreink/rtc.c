20f4a64af6cbb16d78270b4a7a9f98beb26d56cf
Tiemen Molenaar <s1128667@student.windesheim.nl>
2022-05-02 17:06:27 +0200

    now you can setup system time with rtc time and vice versa

---
+++ lib/specific_m5coreink/rtc.c
@@ -0,0 +1,52 @@
+#include <time.h>
+#include <sys/time.h>
+#include "rtc.h"
+#include "bm8563.h"
+#include "i2c_hal.h"
+#include "esp_log.h"
+
+// private global variables
+bm8563_t bm8563;
+
+// initialize rtc library
+void rtc_ainit()
+{
+  // initialize bm8563 functions and i2c hal
+  i2c_hal_init();
+  bm8563.handle = NULL;
+  bm8563.read = i2c_hal_read;
+  bm8563.write = i2c_hal_write;
+  bm8563_init(&bm8563);
+}
+
+// copy system time to rtc
+void rtc_syncronize_rtc_time() {
+ time_t current_time;
+  // set rtc time with system time
+  current_time = time(NULL);
+  bm8563_write(&bm8563, localtime(&current_time));
+}
+
+// copy rtc time to system
+void rtc_syncronize_sys_time()
+{
+ struct tm rtc_time;
+ struct timeval tv = {0, 0};
+ struct timezone tz = {0, 0}; // UTC+0
+  // retrieve rtc time
+  bm8563_read(&bm8563, &rtc_time);
+  tv.tv_sec = mktime(&rtc_time);
+
+  // set system time
+  settimeofday(&tv, &tz);
+}
+
+// reads out the rtc time and print formated string
+void rtc_print_time()
+{
+  char buffer[128];
+  struct tm rtc;
+  bm8563_read(&bm8563, &rtc);
+  strftime(buffer, 128, "%c (day %j)", &rtc);
+  ESP_LOGD("print", "RTC: %s\n", buffer);
+}
