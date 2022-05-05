d36ef30b9dceae949638cd12271e1f1060600f6a
Tiemen Molenaar <s1128667@student.windesheim.nl>
2022-05-05 12:54:42 +0200

    check that rtc library and scheduler library works together, and it did!. And I have added some debug code for performing measurments with PPK2 (that will be removed in future)

--- lib/specific_m5coreink/scheduled_tasks.c
+++ lib/specific_m5coreink/scheduled_tasks.c
@@ -2,13 +2,20 @@
 #include <generic_esp_32.h>
 #include <scheduler.h>
 
+#define PPK2_NUM_D0 GPIO_NUM_25
+#define PPK2_NUM_D1 GPIO_NUM_26
+#define PPK2_NUM_D2 GPIO_NUM_23
+#include <driver/gpio.h>
+
 // test task A
 void taskA(void *in)
 {
+    gpio_set_level(PPK2_NUM_D1,1);
     ESP_LOGD("taskA", "I am task A and i am running");
-    ESP_LOGD("taskA", "I wait 5 seconds");
-    vTaskDelay(pdMS_TO_TICKS(5000));
+    ESP_LOGD("taskA", "I wait 10 seconds");
+    vTaskDelay(pdMS_TO_TICKS(10000));
     ESP_LOGD("taskA", "I have done my purpuse, bye");
+    gpio_set_level(PPK2_NUM_D1,0);
 
     // tell that is stopped
     xEventGroupSetBits(scheduler_taskevents, BIT_TASK(((scheduler_parameter_t *)in)->id));
@@ -18,10 +25,12 @@ void taskA(void *in)
 // test task B
 void taskB(void *in)
 {
+    gpio_set_level(PPK2_NUM_D2,1);
     ESP_LOGD("taskB", "I am task B and i am running");
-    ESP_LOGD("taskB", "I wait 3 seconds");
-    vTaskDelay(pdMS_TO_TICKS(3000));
+    ESP_LOGD("taskB", "I wait 5 seconds");
+    vTaskDelay(pdMS_TO_TICKS(5000));
     ESP_LOGD("taskB", "I have done my purpuse, bye");
+    gpio_set_level(PPK2_NUM_D2,0);
 
     // tell that is stopped
     xEventGroupSetBits(scheduler_taskevents, BIT_TASK(((scheduler_parameter_t *)in)->id));
