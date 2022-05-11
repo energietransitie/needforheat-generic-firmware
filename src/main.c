468d625c28dcb341da1bb06d323374588fbfc6d3
n-vr <43145188+n-vr@users.noreply.github.com>
2022-05-11 12:41:19 +0000

    Adapt OTA update task to new scheduler

--- src/main.c
+++ src/main.c
@@ -65,7 +65,8 @@ void app_main(void)
 
 scheduler_t schedule[] = {
     {taskA, "task a", 4096, {0, NULL}, 1, SCHEDULER_INTERVAL_1M},
-    {taskB, "task b", 4096, {0, NULL}, 1, 120}};
+    {taskB, "task b", 4096, {0, NULL}, 1, 120},
+    {twomes_ota_firmware_update_task, "firmware update", 16384, {0, NULL}, 1, SCHEDULER_INTERVAL_1D}};
 int schedule_size = sizeof(schedule)/sizeof(scheduler_t);
 interval_t wakeup_interval = SCHEDULER_INTERVAL_1M;
 
