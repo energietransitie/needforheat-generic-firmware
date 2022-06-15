05669af3e5c5390170dffda322d448dba8c66b73
Tiemen Molenaar <s1128667@student.windesheim.nl>
2022-06-15 14:24:02 +0200

    Removed test tasks and clean up schedule

--- lib/specific_m5coreink/scheduled_tasks.h
+++ lib/specific_m5coreink/scheduled_tasks.h
@@ -1,12 +1,6 @@
 #ifndef SCHEDULED_TASKS_H
 #define SCHEDULED_TASKS_H
 
-// test tasks
-void taskA(void *);
-void taskB(void *);
-void taskC(void *);
-
-// real tasks
 void heartbeatv2_task(void *arg);
 void upload_task(void *arg);
 void twomes_scd41_task(void *arg);
