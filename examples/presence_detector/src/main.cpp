e18fe3c6142b984fc93de44b35b8ab7dc61bc54a
n-vr <43145188+n-vr@users.noreply.github.com>
2022-08-18 08:16:52 +0000

    Schedule tasks before generic firmware init
    This is necessary for when all scheduled tasks need to be run by initializition.
    This is the case when the device is just provisioned.

--- examples/presence_detector/src/main.cpp
+++ examples/presence_detector/src/main.cpp
@@ -6,11 +6,11 @@ constexpr const char *DEVICE_TYPE_NAME = "Presence-Detector";
 
 extern "C" void app_main(void)
 {
-	GenericESP32Firmware::Initialize(DEVICE_TYPE_NAME);
-
 	// Add the generic tasks to the scheduler,
 	// heartbeat, timesync and optionally presence detection and OTA updates.
 	GenericTasks::AddTasksToScheduler();
+
+	GenericESP32Firmware::Initialize(DEVICE_TYPE_NAME);
 
 	// Start the scheduler.
 	Scheduler::Start();
